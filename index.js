85998f2d5a48a872f4f1c2fb85a84cd6e9c8334b
Pial <pialname394@gmail.com>
2026-06-21 15:42:08 +0600

    connected database with mongodb

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv").config();
-const port = 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,35 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend Server Running On!!");
 });
+
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("arthub_db");
+    const userCollection = db.collection("user");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
